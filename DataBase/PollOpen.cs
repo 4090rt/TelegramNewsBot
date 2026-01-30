d183b79ec99b780a5403910293183b2e92273360
4090rt <artem2007yannurow@gmail.com>
2026-01-30 22:26:12 +0500

    Add files via upload

--- DataBase/PollOpen.cs
+++ DataBase/PollOpen.cs
@@ -9,7 +9,9 @@ namespace TelegramNewsBot.DataBase
 {
     public class PollOpen
     {
-        private readonly Stack<SQLiteConnection> _pool = new Stack<SQLiteConnection>();
+        private readonly Stack<SQLiteConnection> _available = new Stack<SQLiteConnection>();
+        private readonly List<SQLiteConnection> _inUse = new List<SQLiteConnection>();
+        private readonly object _lock = new object();
         private readonly string _dbpath;
         private readonly int _maxCouhnt = 10;
 
@@ -28,38 +30,57 @@ namespace TelegramNewsBot.DataBase
 
         public SQLiteConnection Pollopen()
         {
-            lock (_pool)
+            lock (_lock)
             {
-                if (_pool.Count > 1)
-                { 
-                    _pool.Pop();
-                }
-            }
-            return CreateConnection();
-        }
+                SQLiteConnection conn;
 
-        public void ClosePool(SQLiteConnection connection)
-        {
-            if (connection.State == System.Data.ConnectionState.Broken && connection.State == System.Data.ConnectionState.Closed)
-            {
-                connection.Dispose();
-                return;
-            }
-            lock (_pool)
-            {
-                if (_pool.Count < _maxCouhnt)
+                if (_available.Count > 1)
                 {
-                    _pool.Push(connection);
+                    conn = _available.Pop();
+
+                    if (conn.State != System.Data.ConnectionState.Open)
+                    {
+                        conn.Dispose();
+                        conn = CreateConnection();
+                    }
+                }
+                else if (_available.Count + _inUse.Count < _maxCouhnt)
+                {
+                    conn = CreateConnection();
                 }
                 else
                 {
-                    connection.Close();
-                    connection.Dispose();
+                    throw new Exception("Пул занят!");
+                }
+
+                _inUse.Add(conn);
+                return conn;
+            }
+        }
+
+        public void ClosePool(SQLiteConnection conn)
+        {
+            if (conn == null) return;
+            lock (_lock)
+            {
+                if (_inUse.Contains(conn))
+                {
+                    _inUse.Remove(conn);
+
+                    if (conn.State == System.Data.ConnectionState.Open)
+                    {
+                        _available.Push(conn);
+                    }
+                    else
+                    {
+                        conn.Dispose();
+                    }
+                }
+                else
+                {
+                    throw new Exception("Соединение не найдено");
                 }
             }
         }
-        
-
-
     }
 }
