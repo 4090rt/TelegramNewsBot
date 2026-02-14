264b21a4862712597d780692c5549fdba29d99b0
4090rt <artem2007yannurow@gmail.com>
2026-02-14 23:43:51 +0500

    Add files via upload

---
+++ RequestAndParcing/ModelBse/ModelCrypto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramNewsBot.RequestAndParcing.ModelBse
+{
+    public class ModelCrypto
+    {
+        public CurrencyRate bitcoin { get; set; }
+        public CurrencyRate ethereum { get; set; }
+        public CurrencyRate tether { get; set; }
+    }
+    public class CurrencyRate
+    {
+        public decimal rub { get; set; }
+    }
+}
---
+++ RequestAndParcing/ModelBse/ModelValute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TelegramNewsBot.RequestAndParcing.ModelBse
+{
+    public class ModelValute
+    {
+        [JsonPropertyName("base_code")]
+        public string BaseCode { get; set; }
+
+        [JsonPropertyName("conversion_rates")]
+        public Dictionary<string, decimal> ConversionRates { get; set; }
+    }
+}
